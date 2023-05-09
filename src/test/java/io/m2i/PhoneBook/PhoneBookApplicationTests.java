06f7206a0e664d0fdb452818ae34953b9e1e6095
mhewb <mhe.bousquet@gmail.com>
2023-05-09 09:30:16 +0200

    init commit

---
+++ src/test/java/io/m2i/PhoneBook/PhoneBookApplicationTests.java
@@ -0,0 +1,13 @@
+package io.m2i.PhoneBook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoneBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
